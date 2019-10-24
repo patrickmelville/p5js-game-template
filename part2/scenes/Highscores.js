bbcefebe0d437392c3a60fe10a7318349130030d
Patrick M <pmelville321@gmail.com>
2019-10-24 14:10:44 -0500

    added part2 - fully functional menu system

---
+++ part2/scenes/Highscores.js
@@ -0,0 +1,78 @@
+let Highscores = {
+    setup: function () {
+        // set stuff up
+        this.defaultScores = [1,1,1,1,1,1,1,1,1,1,1,0];
+        this.storedHighscoresName = GM.title;
+
+        // LOADING HIGHSCORES from default or localStorage
+        this.highscores = [];
+        if (localStorage.hasOwnProperty(this.storedHighscoresName)) {
+            this.highscores = JSON.parse(localStorage[this.storedHighscoresName]);
+            console.log("I grabbed HS's from local storage");
+        } else {
+            this.highscores = this.defaultScores;
+            localStorage[this.storedHighscoresName] = JSON.stringify(this.defaultScores);
+            console.log("I grabbed HS's from default scores");
+        }
+        console.log("after: ", this.highscores);
+    },
+    draw: function () {
+        push();
+        background(0);
+        fill(255);
+        textAlign(CENTER);
+        textSize(66);
+        text("HIGHSCORES", width / 2, 200);
+
+        let textStart = 300;
+        textAlign(LEFT);
+        textSize(33);
+        let sortedArray = this.getHighscores();
+        for (let i = 0; i < 10; i++) {
+            let score = sortedArray[i];
+            if (i < 9)
+                text(" " + (i + 1) + ":    " + score, width / 3, textStart);
+            else
+                text(i + 1 + ":    " + score, width / 3, textStart);
+            textStart += 30;
+        }
+
+        textAlign(CENTER);
+        if (GM.recentScore != null) {
+            text("Your most recent score: " + GM.recentScore, width / 2, height - 170);
+        }
+
+        text("press any key to go back...", width / 2, height - 100);
+
+
+
+
+        pop();
+    },
+    update: function () {
+
+    },
+
+    addHighscore: function (score) {
+        let tempScores = JSON.parse(localStorage[this.storedHighscoresName]);
+        tempScores.push(score);
+        localStorage[this.storedHighscoresName] = JSON.stringify(tempScores);
+    },
+
+    getHighscores: function () {
+        let newArray = this.highscores.sort(function (a, b) {
+            return b - a
+        });
+        // sort the scores array from highest to lowest
+
+        return newArray;
+    },
+
+    keyPressed: function (keyCode) {
+        console.log("Highscores detected keyPressed(): " + keyCode);
+        GM.setCurrentScene("MainMenu");
+
+        // this.addHighscore(random(0, 10000));
+        // this.setup();
+    },
+};
